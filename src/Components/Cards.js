7e54cf216173cbac6bcf60a818d596d30f3917be
JMaurizio <90101715+JMaurizio@users.noreply.github.com>
2022-04-17 20:18:30 -0500

    basic work done on project section

---
+++ src/Components/Cards.js
@@ -0,0 +1,19 @@
+import { Card } from "react-bootstrap";
+
+function Cards(props) {
+    return(
+        <Card id={props.project.id} className="w-25 m-4">
+            <Card.Img variant="top" src={props.project.image} />
+            <Card.Body>
+                <Card.Title>{props.project.name}</Card.Title>
+                <Card.Text>{props.project.description}</Card.Text>
+            </Card.Body>
+            <Card.Body>
+                <Card.Link href={props.project.github}>Github</Card.Link>
+                <Card.Link href={props.project.url}>Live Link</Card.Link>
+            </Card.Body>
+        </Card>
+    )
+};
+
+export default Cards;
--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,8 +1,45 @@
-
+import { useState } from "react";
+import Cards from "../Components/Cards";
+import programmer from "../assets/images/programmer-dashboard.png";
+import chattleship from "../assets/images/chattleship-board-crop.png";
+import catro from "../assets/images/catro-logo.png";
 
 function Projects() {
+    const [projectInfo] = useState([
+        {
+            name: 'Catro Gaming',
+            github: 'https://github.com/JoelKovalcson/Catro-Gaming',
+            image: catro,
+            url: 'https://catro-gaming.herokuapp.com/',
+            description: 'A retro style arcade with single and multiplayer games(more soon to come).',
+            id: 'catro'
+        },
+        {
+            name: 'Chattleship',
+            github: 'https://github.com/JoelKovalcson/ChattleShip',
+            image: chattleship,
+            url: 'https://chattleship.herokuapp.com/',
+            description: 'A Battleship like game with real time chat functionality.',
+            id: 'chattleship'
+        },
+        {
+            name: 'Programmer Dashboard',
+            github: 'https://github.com/JoelKovalcson/Programmer-Dashboard',
+            image: programmer,
+            url: 'https://joelkovalcson.github.io/Programmer-Dashboard/',
+            description: 'A webpage where programmers can view their github feed and hacker news.',
+            id: 'programmer'
+        },
+    ]);
+
+    const project = [];
+    for(let i = 0; i < projectInfo.length; i++) {
+        project.push(<Cards key={`Project-${i}`} project={projectInfo[i]}/>)
+    }
     return(
-        <div>Projects</div>
+        <div className="d-flex flex-wrap">
+            {project}
+        </div>
     )
 };
 
