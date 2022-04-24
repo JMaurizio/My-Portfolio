22f59f4c4f3098076942a8af60cf3133ef22fedf
JMaurizio <90101715+JMaurizio@users.noreply.github.com>
2022-04-24 17:42:06 -0500

    final stylings

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -30,11 +30,11 @@ function Header({page, setPage}) {
         <div className="nav">
             <input type="checkbox" className="nav-checkbox" id="nav-toggle"></input>
             <label for="nav-toggle" className="nav-button">
-                <span className="nav-icon">
+                <div className="nav-icon">
                     <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
                     <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
                     </svg>
-                </span>
+                </div>
             </label>
             <div className="nav-background"></div>
 
